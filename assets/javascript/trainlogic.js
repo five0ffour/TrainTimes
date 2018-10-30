6ef5bbb3db798dbceafa7c596b6b14317bea85d3
Mike Galarneau <mbgalarneau@gmail.com>
2018-10-29 21:36:39 -0400

    added timer logic to refresh display every 15 seconds

--- assets/javascript/trainlogic.js
+++ assets/javascript/trainlogic.js
@@ -14,6 +14,14 @@ firebase.initializeApp(config);
 var database = firebase.database();
 var trains = [];
 
+// Timer variables
+var displayTimerId = 0;             // display timer id for setInterval/clearInterval
+var displayTimerRunning = false;    // flags for the wait timer so we don't spawn multiple timers  
+const displayInterval = 15;         // Update the display four times / minute
+
+// Fire off the timer event to refresh the display periodically
+startDisplayTimer();
+
 //---------------------------------
 // Button handler for adding Trains
 //---------------------------------
@@ -37,13 +45,15 @@ $("#add-train-btn").on("click", function (event) {
   // Write train data to the database
   database.ref().push(newTrain);
 
-  // Logs everything to console for debugging
+  // Logs train state to console for debugging
+  console.log("------------on click add event ---------------");
   console.log(newTrain.name);
   console.log(newTrain.destination);
   console.log(newTrain.firstTime);
   console.log(newTrain.frequency);
+  console.log("----------------------------------------------");
 
-  alert("Train Schedule successfully added");
+  // alert("Train Schedule successfully added");
 
   // Clears the text-boxes
   $("#train-name-input").val("");
@@ -64,41 +74,117 @@ database.ref().on("child_added", function (childSnapshot) {
   var trnFirst = childSnapshot.val().firstTime;
   var trnFreq = childSnapshot.val().frequency;
 
-  // Train Info
-  console.log(trnName);
-  console.log(trnDest);
-  console.log("First Time: " + trnFirst);
-  console.log("Frequency: " + trnFreq);
+  // Train Info - log for debugging
+  console.log("------------ on child added event ---------------");
+  // console.log(trnName);
+  // console.log(trnDest);
+  // console.log("First Time: " + trnFirst);
+  // console.log("Frequency: " + trnFreq);
+  // console.log("-------------------------------------------------");
 
   // Calculate the arrival of the next train
   var trnNextArrival = calculateNextArrivalTime(trnFirst, trnFreq);
 
   // Calculate the duration to the next train
-  var duration = moment.duration(trnNextArrival.diff(moment()));
-  var trnMinutesAway = Math.round(duration.asMinutes());
+  var trnMinutesAway = calculateMinutesToArrival(trnNextArrival);
 
   // Save the read train data and additional metadata into a sorted array of train objects
-  // var train = {};
-  // train.name = trnName;
-  // train.destination = trnDest;
-  // train.firstTime = trnFirst;
-  // train.frequency = trnFreq;
-  // train.minutesAway = parseInt(trnMinutesAway);
-  // trains.push(train);
-  // trains.sort(function(a,b) { return a.minutesAway - b.minutesAway; } );
+  var train = {};
+  train.name = trnName;
+  train.destination = trnDest;
+  train.firstTime = trnFirst;
+  train.frequency = trnFreq;
+  trains.push(train);
+
+  // Refresh the board with the newly added train (although the display timer will do this too)
+  updateBoardDisplay(trains);
+});
+
+//------------------------
+// startDisplayTimer()  - kicks off an interval to vamp until we want to update the display board
+//------------------------
+function startDisplayTimer() {
+  if (!displayTimerRunning) {
+    displayTimerRunning = true;
+    displayTimerId = setInterval(updateDisplayTimer, displayInterval * 1000);
+  }
+}
+
+//------------------------
+// stopDisplayTimer()  - cancels the update timer, never needed to be called in this app for the moment
+//------------------------
+function stopDisplayTimer() {
+  if (displayTimerRunning) {
+    clearInterval(displayTimerId);
+    displayTimerRunning = false;
+  }
+}
+
+//---------------------
+// updateDisplayTimer() - periodically refresh the display board
+//----------------------
+function updateDisplayTimer() {
+  console.log("updateDisplayTimer() -- refreshing display"); 
+  updateBoardDisplay(trains);
+}
+
+
+function calculateMinutesToArrival(arrivalTime) {
+  var duration = moment.duration(arrivalTime.diff(moment()));
+  return Math.round(duration.asMinutes());
+}
+
+//------------------------------------
+// updateBoarDisplay(trains) - main UI logic for this application.  
+//        1. query the passed array of train objects
+//        2. calculate and store the metadata (e.g., next arrival time)
+//        3. sort the array by metadata
+//        3. clear the current display board
+//        4. reconstruct the UI table with the data/metadata in sorted order
+//
+//------------------------------------
+function updateBoardDisplay(trains) {
+
+  // Compute and store new arrival times in train item
+  for (var i = 0;
+    (i < trains.length); i++) {
+    var train = trains[i];
+    train.nextArrival = calculateNextArrivalTime(train.firstTime, train.frequency);
+    train.minutesAway = calculateMinutesToArrival(train.nextArrival);
+  }
+
+  // Sort the master train array by arrival times 
+  trains.sort(function (a, b) {
+    return a.minutesAway - b.minutesAway;
+  });
+
+  // Empty the display board
+  $("#train-table > tbody").empty();
+
+  // Add the trains back to the board
+  for (var i = 0;
+    (i < trains.length); i++) {
+    addTrainToBoard(trains[i]);
+  }
+}
+
+//------------------------------------
+// addTrainToBoard(train) - create/attach a row of the UI display board table
+//------------------------------------
+function addTrainToBoard(train) {
 
   // Create a new table row and populate with the formatted data
   var newRow = $("<tr>").append(
-    $("<td>").text(trnName),
-    $("<td>").text(trnDest),
-    $("<td>").text(trnFreq),
-    $("<td>").text(trnNextArrival.format('hh:mm A')),
-    $("<td>").text(trnMinutesAway)
+    $("<td>").text(train.name),
+    $("<td>").text(train.destination).addClass("col-city"),
+    $("<td>").text(numberWithCommas(train.frequency)).addClass("col-number"),
+    $("<td>").text(train.nextArrival.format('MM/DD hh:mm A')).addClass("col-time"),
+    $("<td>").text(numberWithCommas(train.minutesAway)).addClass("col-number")
   );
 
   // Append the new row to the DOM table
   $("#train-table > tbody").append(newRow);
-});
+}
 
 //---------------------------------
 // calculteNextArrivalTime() - Use the first Train time, the frequency and current time to determine when the 
@@ -124,7 +210,16 @@ function calculateNextArrivalTime(startTime, frequencyMinutes) {
   }
 
   // Log the result for debugging
-  console.log("calculateNextArrivalTime(): " + nextArrivalTime.format("HH:mm A", "X"));
+  // console.log("calculateNextArrivalTime(): " + nextArrivalTime.format("MM/DD HH:mm A", "X"));
 
   return nextArrivalTime;
+}
+
+/*************************/
+/*  Utility Functions    */
+/*************************/
+
+// numbersWithCommas(x) - format the number to a string with commas (presumably for display)
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
